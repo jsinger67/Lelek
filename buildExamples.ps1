810c32ea2dcf31f69e5bb5a11da00e934ea63d36
Jörg Singer <joerg.singer@outlook.de>
2019-07-28 13:15:12 +0200

    Extended  buildExamples.ps1.

--- buildExamples.ps1
+++ buildExamples.ps1
@@ -8,6 +8,7 @@ $SolutionDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 Set-Location ${SolutionDir}\src\Lelek
 
+# --------------------------------------------------------------------------------------------------
 Write-Host "Building List example..." -ForegroundColor Cyan
 dotnet run -- --file ${SolutionDir}\grammars\lst.llk --generate-lexersrc ${SolutionDir}\examples\List\ListLexer.fs --generate-parsersrc ${SolutionDir}\examples\List\ListParser.fs --save-linear-grammar --asttype AST.AST
 if ($LASTEXITCODE -ne 0) {
@@ -15,18 +16,30 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 
+# --------------------------------------------------------------------------------------------------
 Write-Host "Building Calc example..." -ForegroundColor Cyan
 dotnet run -- --file ${SolutionDir}\grammars\Calc.llk --generate-lexersrc ${SolutionDir}\examples\Calc\CalcLexer.fs --generate-parsersrc ${SolutionDir}\examples\Calc\CalcParser.fs --save-linear-grammar --asttype AST.AST
 if ($LASTEXITCODE -ne 0) {
     ++$ErrorCont    
 }
 
+# --------------------------------------------------------------------------------------------------
 Write-Host "Building Oberon-2 example..." -ForegroundColor Cyan
 dotnet run -- --file ${SolutionDir}\grammars\oberon-2.llk --generate-lexersrc ${SolutionDir}\examples\oberon2\Oberon2Lexer.fs --generate-parsersrc ${SolutionDir}\examples\oberon2\Oberon2Parser.fs --save-linear-grammar --asttype AST.AST --srcnamespacename Oberon2 --lexermodulename Oberon2Lexer --parsermodulename Oberon2Parser
 if ($LASTEXITCODE -ne 0) {
     ++$ErrorCont    
 }
 
+# --------------------------------------------------------------------------------------------------
+Write-Host "Building BooleanParser example..." -ForegroundColor Cyan
+dotnet run -- --file ${SolutionDir}\examples\BooleanParser\BooleanParser.llk --generate-lexersrc ${SolutionDir}\examples\BooleanParser\Lexer.fs --generate-parsersrc ${SolutionDir}\examples\BooleanParser\Parser.fs --save-linear-grammar --asttype AST.AST --srcnamespacename BP --lexermodulename BPLexer --parsermodulename BPParser
+if ($LASTEXITCODE -ne 0) {
+    ++$ErrorCont    
+}
+
+# --------------------------------------------------------------------------------------------------
+# Final message
+# --------------------------------------------------------------------------------------------------
 if ($ErrorCont -gt 0) {
     $Msg = "$ErrorCount error(s) occurred."
     Write-Host -Object $Msg  -ForegroundColor Red
